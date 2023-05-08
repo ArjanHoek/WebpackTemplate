9a2078e954109fe2993ae6a0d2f599206efbde36
Arjan Hoek <arjanhoek5191@gmail.com>
2023-05-08 21:09:46 +0200

    add some rxjs examples

--- src/rxjs/index.ts
+++ src/rxjs/index.ts
@@ -1,3 +1,16 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, scan, take, throttleTime } from 'rxjs';
 
-fromEvent(document, 'click').subscribe(e => console.log(e));
+// RxJS is able to produce values using pure functions, making code less prone to errors.
+
+const observable = fromEvent(document, 'click').pipe(
+  throttleTime(1000), // Make sure that there is at least 1000 seconds between each subsequent event firing
+  scan(count => count + 1, 0) // The 'scan' operator works like the Array object's 'reduce' method
+);
+
+const subscription = observable.subscribe(count =>
+  console.log(`Clicked ${count} times`)
+);
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 10000);
